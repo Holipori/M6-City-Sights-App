ead699ec41569327d5adbb32372d1218ddb682d3
Holipori <43333502+Holipori@users.noreply.github.com>
2022-10-28 13:17:15 -0500

    Initial Commit

---
+++ M6 City Sights App/M6_City_Sights_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  M6_City_Sights_AppApp.swift
+//  M6 City Sights App
+//
+//  Created by Xinyue Hu on 10/28/22.
+//
+
+import SwiftUI
+
+@main
+struct M6_City_Sights_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
